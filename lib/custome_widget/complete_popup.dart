5ef16ab6033e03ca27235bb7f0b462bc7314f094
sajidh <sajidhshaji4@gmail.com>
2025-08-08 00:12:05 +0530

    completed confirmation popup added

--- lib/custome_widget/complete_popup.dart
+++ lib/custome_widget/complete_popup.dart
@@ -1,31 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:parknet_pro/controller/booking_controller.dart';
+import 'package:parknet_pro/utils/app_colors.dart';
 
-Future<void> showCompleteConfirmationDialog(
-  BuildContext context,
-  String bookingId,
-) async {
-  return showDialog<void>(
+void showCompleteConfirmationDialog(BuildContext context, String bookingId) {
+  showGeneralDialog(
     context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Complete Booking'),
-        content: const Text('Are you sure you want to complete this booking?'),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('No'),
-            onPressed: () => Navigator.of(context).pop(),
+    pageBuilder: (context, animation1, animation2) {
+      return Container(); // Required by showGeneralDialog, but unused
+    },
+    transitionDuration: const Duration(milliseconds: 300),
+    transitionBuilder: (context, a1, a2, widget) {
+      return ScaleTransition(
+        scale: Tween<double>(begin: 0.5, end: 1.0).animate(a1),
+        child: FadeTransition(
+          opacity: Tween<double>(begin: 0.5, end: 1.0).animate(a1),
+          child: AlertDialog(
+            backgroundColor: AppColors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(13.0),
+            ),
+            title: const Text(
+              'Complete Booking',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            content: const Text(
+              'Are you sure you want to complete this booking?',
+              style: TextStyle(fontSize: 16),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text(
+                  'No',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  final controller = Get.find<BookingController>();
+                  controller.completeBooking(bookingId);
+                },
+                child: const Text(
+                  'Yes',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
           ),
-          TextButton(
-            child: const Text('Yes'),
-            onPressed: () {
-              Navigator.of(context).pop();
-              final controller = Get.find<BookingController>();
-              controller.completeBooking(bookingId);
-            },
-          ),
-        ],
+        ),
       );
     },
   );
